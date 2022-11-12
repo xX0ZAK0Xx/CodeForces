def0ebe82f14f136501d8061bff984353ac9ebda
Zayed_Oyshik <zayedoyshik1@gmail.com>
2022-11-12 22:55:30 +0600

    first commit

---
+++ 833/a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define nfs ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define test int t;cin>>t;while(t--)
+#define print(n) cout<<(n)<<"\n";
+#define printarray(arr) for(auto xx: (arr) ){cout<<xx<<" ";} cout<<"\n";
+#define print2darray(arr) for(auto yy: (arr)){printarray(yy)}
+#define ll long long int
+#define  d2vec(name, row, col, fill) vector<vector<int>> (name) ((row), vector<int>((col),(fill)));
+#define  D2vec(name, row, col, fill) vector<vector<ll>> (name) ((row), vector<ll>((col),(fill)));
+#define rowfill(arr, col, val) for(ll xx=0 ; xx<(col) ; xx++){ arr[0][xx]=(val);}
+#define colfill(arr, row, val) for(ll xx=0 ; xx<(row) ; xx++){ arr[xx][0]=(val);}
+using namespace std;
+int main(){
+
+    nfs test{
+        double n; cin >> n;
+        cout<< (ll)ceil(n/2) <<"\n";
+    }
+
+    return 0;
+}
