9c7a856b6062068f2ad5f9dea31defcef9192e86
Zayed_Oyshik <zayedoyshik1@gmail.com>
2022-12-10 12:25:01 +0600

    d19

---
+++ practice/900_NITDestroys.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define nfs ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define test int t;cin>>t;while(t--)
+#define print(n) cout<<(n)<<"\n";
+#define printarray(arr) for(auto xx: (arr) ){cout<<xx<<" ";} cout<<"\n";
+#define print2darray(arr) for(auto yy: (arr)){printarray(yy)}
+#define ll long long int
+#define  d2vec(name, row, col, fill) vector<vector<int>> (name) ((row), vector<int>((col),(fill)));
+#define  D2vec(name, row, col, fill) vector<vector<ll>> (name) ((row), vector<ll>((col),(fill)));
+#define rowfill(arr, col, val) for(ll xx=0 ; xx<(col) ; xx++){ arr[0][xx]=(val);}
+#define colfill(arr, row, val) for(ll xx=0 ; xx<(row) ; xx++){ arr[xx][0]=(val);}
+using namespace std;
+int main(){
+
+    nfs test{
+        int n; cin  >> n;
+        vector<int> v(n);
+        for(int i=0; i<n; i++) cin >> v[i];
+        
+        while(v.size()>0 && v[0]==0) v.erase(v.begin());
+        while(v.size()>0 && v[v.size()-1]==0) v.pop_back();
+
+        int ans = 0;
+        for(int i=0; i<v.size(); i++){
+            if(v[i]==0) ans++;
+        }
+        if(v.size()==0) print(0)
+        else if(ans!=0) print(2)
+        else print(1)
+    }
+        
+
+    return 0;
+}
