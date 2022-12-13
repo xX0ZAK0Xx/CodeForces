712189f737635ee88fbb0fe60e96f9976dfd339a
Zayed_Oyshik <zayedoyshik1@gmail.com>
2022-12-13 21:43:19 +0600

    d20

---
+++ practice/900_AvtoBus.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define nfs ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define test int t;cin>>t;while(t--)
+#define print(n) cout<<(n)<<"\n";
+#define printarray(arr) for(auto xx: (arr) ){cout<<xx<<" ";} cout<<"\n";
+#define print2darray(arr) for(auto yy: (arr)){printarray(yy)}
+#define ll long long int
+#define  d2vec(name, row, col, fill) vector<vector<int>> (name) ((row), vector<int>((col),(fill)));
+#define  D2vec(name, row, col, fill) vector<vector<ll>> (name) ((row), vector<ll>((col),(fill)));
+#define rowfill(arr, col, val) for(ll xx=0 ; xx<(col) ; xx++){ arr[0][xx]=(val);}
+#define colfill(arr, row, val) for(ll xx=0 ; xx<(row) ; xx++){ arr[xx][0]=(val);}
+using namespace std;
+int main(){
+
+    nfs test{
+        ll n; cin >> n;
+        // ll mm, mx;
+        if(n<=2){
+            print(-1)
+            continue;
+        }
+        if((n%6)&1){
+            print(-1)
+        }else{
+            if((n%6)){
+                cout<<(n/6 + 1)<<" ";
+            }else{
+                cout<<(n/6)<<" ";
+            }
+
+            print(n/4)
+        }
+    }
+
+    return 0;
+}
