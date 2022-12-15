eda7494b5c51fcaf20c42500983816db1c7ae0ee
Zayed_Oyshik <zayedoyshik1@gmail.com>
2022-12-15 23:16:29 +0600

    d22

---
+++ practice/900_Football.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define nfs ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define test int t;cin>>t;while(t--)solve();
+#define print(n) cout<<(n)<<"\n";
+#define printarray(arr) for(auto xx: (arr) ){cout<<xx<<" ";} cout<<"\n";
+#define print2darray(arr) for(auto yy: (arr)){printarray(yy)}
+#define ll long long int
+#define  d2vec(name, row, col, fill) vector<vector<int>> (name) ((row), vector<int>((col),(fill)));
+#define  D2vec(name, row, col, fill) vector<vector<ll>> (name) ((row), vector<ll>((col),(fill)));
+#define rowfill(arr, col, val) for(ll xx=0 ; xx<(col) ; xx++){ arr[0][xx]=(val);}
+#define colfill(arr, row, val) for(ll xx=0 ; xx<(row) ; xx++){ arr[xx][0]=(val);}
+using namespace std;
+bool solve(string s){
+    bool flag = false;
+    int ctr = 0;
+    for(int i=0 ; i<s.length()-1 ; i++){
+        if(s[i]==s[i+1]){
+            ctr++;
+        }
+        if(ctr>=7){
+            return true;
+        }
+    }
+    return false;
+}
+int main(){
+
+    string s; cin>>s;
+
+    if(solve(s)){
+        print("YES")
+    }else{
+        print("NO")
+    }
+
+
+    return 0;
+}
